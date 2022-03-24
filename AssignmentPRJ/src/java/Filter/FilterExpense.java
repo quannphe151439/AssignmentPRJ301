7c8bd653f57c7005bea4bb721b86c99bfcdae7d0
quannphe151439 <quannphe151439@fpt.edu.vn>
2022-03-24 20:32:21 +0700

    update error 403

---
+++ AssignmentPRJ/src/java/Filter/FilterExpense.java
@@ -0,0 +1,91 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Filter;
+
+import dal.AccountDBContext;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import model.Account;
+
+/**
+ *
+ * @author Quan
+ */
+public class FilterExpense implements Filter {
+    
+     @Override
+    public void init(FilterConfig argo) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        request.setCharacterEncoding("UTF-8");
+        response.setCharacterEncoding("UTF-8");
+        PrintWriter out = response.getWriter();
+        if (isAuthenticated((HttpServletRequest) request)) {
+            chain.doFilter(request, response);
+        } else {
+            
+            request.getRequestDispatcher("view/Page403.jsp").forward(request, response);
+        }
+    }
+
+    private boolean isAuthenticated(HttpServletRequest request) {
+        HttpSession session = request.getSession();
+        Account account = (Account) session.getAttribute("account");
+        if (account == null) {
+            Cookie[] cookies = request.getCookies();
+            if (cookies != null)//not login, some cookies
+            {
+                String username = null;
+                String password = null;
+                for (Cookie cooky : cookies) {
+                    if (cooky.getName().equals("username")) {
+                        username = cooky.getValue();
+                    }
+                    if (cooky.getName().equals("password")) {
+                        password = cooky.getValue();
+                    }
+                }
+                if (username == null || password == null) {
+                    return false;
+                } else {
+                    AccountDBContext db = new AccountDBContext();
+                    account = db.getAccount(username, password);
+                    if (account != null) {
+                        request.getSession().setAttribute("account", account);
+                        return true;
+                    } else {
+                        return false;
+                    }
+                }
+            } else //not login, not cookie
+            {
+                return false;
+            }
+        }
+        return true;
+
+    }
+
+    @Override
+    public void destroy() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
